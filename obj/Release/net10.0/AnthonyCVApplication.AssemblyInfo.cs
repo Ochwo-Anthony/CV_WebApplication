05aebdcf70c176fcb00095ee58efb95b44b5027e
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2026-02-23 20:41:39 +0200

    Updated Layout file

--- obj/Release/net10.0/AnthonyCVApplication.AssemblyInfo.cs
+++ obj/Release/net10.0/AnthonyCVApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnthonyCVApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9589b8dd034a5657c17467b7fd7d15c3816cee05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc580fa64b0e4936bea26bee100373308f0c2fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnthonyCVApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnthonyCVApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
